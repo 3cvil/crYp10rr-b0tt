762c8c3e2da72b4629ef431645970e1637112b49
3cvil <la7121551@gmail.com>
2022-04-19 20:16:50 +0100

    Initial Commit - V.1.0.0

---
+++ events/ready.js
@@ -0,0 +1,39 @@
+const chalk = require('chalk');
+
+module.exports = {
+  name: 'ready',
+  execute(client) {
+    console.log(chalk.green('[BlackKnight683]') + chalk.cyan(' Thanks for using Ticket v2'))
+    console.log(chalk.red('=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+='))
+    console.log(chalk.green('Name: ') + chalk.cyan('Tickety v2'))
+    console.log(chalk.green('Bot Status: ') + chalk.cyan('Initialized'))
+    console.log(chalk.red('=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+='))
+    const oniChan = client.channels.cache.get(client.config.ticketChannel)
+
+    function sendTicketMSG() {
+      const embed = new client.discord.MessageEmbed()
+        .setColor('6d6ee8')
+        .setAuthor('Create an order', client.user.avatarURL())
+        .setDescription('To create an order, press the 💸 button')
+        .setFooter(`${client.user.tag}`, client.user.displayAvatarURL())
+      const row = new client.discord.MessageActionRow()
+        .addComponents(
+          new client.discord.MessageButton()
+          .setCustomId('open-ticket')
+          .setLabel('Open a ticket')
+          .setEmoji('💸')
+          .setStyle('PRIMARY'),
+        );
+
+      oniChan.send({
+        embeds: [embed],
+        components: [row]
+      })
+    }
+
+    oniChan.bulkDelete(100).then(() => {
+      sendTicketMSG()
+      console.log(chalk.green('[Tickety v2]') + chalk.cyan(' Sent the ticket creation widget..'))
+    })
+  },
+};
